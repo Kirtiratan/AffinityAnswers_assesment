60f08c19897fd6e9c5ec63cf57e9917b073bf5e9
Kirtiratan <56842057+Kirtiratan@users.noreply.github.com>
2021-12-10 12:57:26 +0530

    Update Degree_of_Profanity.py

--- Degree_of_Profanity.py
+++ Degree_of_Profanity.py
@@ -1,14 +1,14 @@
-import re                                              #this is Python library
+import re                                              #importing useful Python library
 
 def tokenize(text):                                    #text here is the Instagram comment for which we want to check degree
 return re.findall(r'\w+', text.lower())
 
-profane_tokens = {"nerfherder", "shit"}                        #we have to provide few words which can be profane in the list
+profane_tokens = {"nerfherder", "shit"}                        #we have to provide few words which can be profane in this list
 
 insta_comment = "Why you stuck-up, half-witted, scruffy-looking nerfherder!"                #let's say this is one of comment
 
-tokens = tokenize(insta_comment)
+tokens = tokenize(insta_comment)                       #comment is converted into list of words
 
                                                        # Rate: number of occurrences normalized by total number
-degree_of_profanity = sum(1 for t in tokens if t in profane) / len(tokens)
+degree_of_profanity = sum(1 for t in tokens if t in profane_tokens) / len(tokens)
 print(degree_of_profanity)
